4fa73f77f6540ebd77b55299344005504959f18b
HueyPark <jaewan.huey.park@gmail.com>
2016-08-02 23:24:01 +0900

    Add: Castle spawn Dino

--- Code/Source/OpenFire/Framework/GamaObject/Building/Castle/Castle.cpp
+++ Code/Source/OpenFire/Framework/GamaObject/Building/Castle/Castle.cpp
@@ -1,2 +1,20 @@
 #include "OpenFire.h"
 #include "Castle.h"
+#include "Framework/GamaObject/Unit/Dino/Dino.h"
+
+const float SpawnDelaySeconds = 5.0f;
+
+ACastle::ACastle()
+{
+	this->SpawnRemainSeconds = SpawnDelaySeconds;
+}
+
+void ACastle::Tick(float DeltaSeconds)
+{
+	this->SpawnRemainSeconds -= DeltaSeconds;
+	if (this->SpawnRemainSeconds < 0.0f)
+	{
+		GetWorld()->SpawnActor<ADino>(this->CLASS_Dino, this->GetActorLocation(), FRotator::ZeroRotator);
+		this->SpawnRemainSeconds += SpawnDelaySeconds;
+	}
+}
--- Code/Source/OpenFire/Framework/GamaObject/Building/Castle/Castle.h
+++ Code/Source/OpenFire/Framework/GamaObject/Building/Castle/Castle.h
@@ -3,8 +3,22 @@
 #include "GameObject/Building/Building.h"
 #include "Castle.generated.h"
 
+class ADino;
+
 UCLASS()
 class OPENFIRE_API ACastle : public ABuilding
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<ADino> CLASS_Dino;
+
+private:
+	float SpawnRemainSeconds;
+
+public:
+	ACastle();
+
+	virtual void Tick(float DeltaSeconds) override;
 };
